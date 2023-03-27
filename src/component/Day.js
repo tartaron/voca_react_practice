6afdaf1972e4df35abd73853072b995b6e2d6331
tartaron <mon_malloc@berkeley.edu>
2023-03-27 17:22:42 +0900

    add 'goto the word before/after button

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,11 +1,19 @@
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import useFetch from "../hook/useFetch";
 import Word from "./Word";
 
 export default function Day() {
     const {day} = useParams();
-
     const words = useFetch(`http://localhost:3001/words?day=${day}`);
+    const navigate = useNavigate();
+
+    function gotoTheDayBefore() {
+        navigate(`/day/${Number(day) - 1}`);
+    }
+
+    function gotoTheDayAfter() {
+        navigate(`/day/${Number(day) + 1}`);
+    }
 
     return (
         <>
@@ -18,6 +26,12 @@ export default function Day() {
                 })}
                 </tbody>
             </table>
+            <button onClick={gotoTheDayBefore}>
+                goto the day before
+            </button>
+            <button onClick={gotoTheDayAfter}>
+                goto the day after
+            </button>
         </>
     );
 }
